072f8b034321759c17c9248db5f72d3e411e5eca
pictos <pedrojesus.cefet@gmail.com>
2022-02-03 12:30:30 -0300

    bug repro

---
+++ MauiBugRepro/Behaviors/RemoveBorder.android.cs
@@ -0,0 +1,46 @@
+﻿using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.Graphics.Drawables.Shapes;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using AView = Android.Views.View;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maui.Platform;
+
+namespace MauiBugRepro.Behaviors;
+sealed partial class RemoveBorder
+{
+	Drawable? originalBackground;
+    public partial void OnPlatformAttached(View view)
+    {
+        //With this works
+        //await Task.Delay(5000);
+        var mauiContext = view.Handler?.MauiContext; // Handler and MauiContext will be null here
+        var nativeControl = view.ToNative(mauiContext);
+        originalBackground = nativeControl.Background;
+
+        var shape = new ShapeDrawable(new RectShape());
+
+        if (shape.Paint is not null)
+        {
+            shape.Paint.Color = global::Android.Graphics.Color.Transparent;
+            shape.Paint.StrokeWidth = 0;
+            shape.Paint.SetStyle(Paint.Style.Stroke);
+        }
+
+        nativeControl.Background = shape;
+    }
+    public partial void OnPlatformDetached(View view)
+	{
+		var nativeControl = view.Background.ToNative(view.Handler.MauiContext!);
+
+		if (nativeControl is null)
+			return;
+
+		nativeControl.Background = originalBackground;
+	}
+}
+
---
+++ MauiBugRepro/Behaviors/RemoveBorderBehavior.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace MauiBugRepro.Behaviors;
+public partial class RemoveBorder : Behavior<View>
+{
+	protected override void OnAttachedTo(View bindable)
+	{
+		base.OnAttachedTo(bindable);
+		OnPlatformAttached(bindable);
+	}
+
+	protected override void OnDetachingFrom(View bindable)
+	{
+		base.OnDetachingFrom(bindable);
+		OnPlatformDetached(bindable);
+	}
+
+	public partial void OnPlatformAttached(View view);
+
+	public partial void OnPlatformDetached(View view);
+}
